67173e15ecdd36bbe6f77290b52ee270810e3585
Abhishek Arora <abhishek9797@gmail.com>
2020-04-13 03:53:19 +0530

    Logo Fix

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,21 +1,25 @@
 import React from 'react';
 
 const IconLogo = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+  <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 96 96">
     <title>Logo</title>
-    <g id="Logo" transform="translate(-8.000000, -2.000000)">
-      <g transform="translate(13.000000, -2.000000) scale(0.8 0.8)">
+    <g id="B" transform="translate(13.000000, -2.000000) scale(0.8 0.8)">
         <path
           d="m 20.5 90.5 L 40.5 30.5 L 50.5 30.5 L 70.5 90.5 L 60.5 90.5 L 45.5 40.5 L 30.5 90.5 L 20.5 90.5 M 38 65.5 l 15 0 l 2.8 10 l -21 0 z "
           fill="#64FFDA"
         />
-        <polygon
-          id="Shape"
-          stroke="#64FFDA"
-          strokeWidth="5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          points="39 0 0 22 0 67 39 90 78 68 78 23"
+      </g>
+        <path
+        stroke="#64FFDA"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M 50, 5
+                  L 11, 27
+                  L 11, 72
+                  L 50, 95
+                  L 89, 73
+                  L 89, 28 z"
         />
       </g>
     </g>
