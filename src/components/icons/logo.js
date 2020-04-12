29144309d6cffb8484347fdb0145632a73f65a3a
Abhishek Arora <abhishek9797@gmail.com>
2020-04-13 03:58:55 +0530

    Bug fixes

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -3,6 +3,7 @@ import React from 'react';
 const IconLogo = () => (
   <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 96 96">
     <title>Logo</title>
+  <g>
     <g id="B" transform="translate(13.000000, -2.000000) scale(0.8 0.8)">
         <path
           d="m 20.5 90.5 L 40.5 30.5 L 50.5 30.5 L 70.5 90.5 L 60.5 90.5 L 45.5 40.5 L 30.5 90.5 L 20.5 90.5 M 38 65.5 l 15 0 l 2.8 10 l -21 0 z "
@@ -21,7 +22,7 @@ const IconLogo = () => (
                   L 89, 73
                   L 89, 28 z"
         />
-      </g>
+      
     </g>
   </svg>
 );
