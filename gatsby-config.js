2efac568e2147c4e3239a0b14cf0604a5648db3e
Abhishek Arora <1289985+abhishekarora9@users.noreply.github.com>
2020-12-29 23:38:13 +0530

    Bug Fux

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,20 +33,7 @@ module.exports = {
         path: `${__dirname}/content/`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/posts`,
-        name: `posts`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/projects`,
-        name: `projects`,
-      },
-    },
+
     {
       resolve: `gatsby-transformer-remark`,
       options: {
